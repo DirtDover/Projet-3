94309eea922668a5523421504e4b38661b9b68ca
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-01-12 16:23:15 +0100

    Essai de fetch

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,22 @@
-console.log(fetch("http://localhost:5678/api/works"))
+let workData = [];
 
-alert("coucou")
+const fetchWork = async () => {
+    await fetch("http://localhost:5678/api/works")
+        .then((res) => res.json())
+        .then((promise) => {
+            workData = promise;
+            console.log(workData);
+        });
+};
+
+const workdisplay = async () => {
+    await fetchWork();
+
+    document.getElementById("gallery").innerHTML = `<div><img src="${workData[0].imageUrl}" alt="image appartement/></div>`;
+
+
+};
+
+
+
+workdisplay();
