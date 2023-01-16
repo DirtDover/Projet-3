f1b50f8874a559a3c39636b7ea90bd57edc9ce39
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-01-16 11:45:37 +0100

    Recup works

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,22 +1,44 @@
-let workData = [];
-
-const fetchWork = async () => {
-    await fetch("http://localhost:5678/api/works")
-        .then((res) => res.json())
-        .then((promise) => {
-            workData = promise;
-            console.log(workData);
-        });
+async function getWorks() {
+    let rep = await fetch("http://localhost:5678/api/works");
+    let reponse = await rep.json();
+    return reponse;
 };
 
-const workdisplay = async () => {
-    await fetchWork();
+async function generateCard() {
 
-    document.getElementById("gallery").innerHTML = `<div><img src="${workData[0].imageUrl}" alt="image appartement/></div>`;
+    let allWork = await getWorks("http://localhost:5678/api/works")
 
+    for (let i = 0; i < allWork.length; i++) {
 
+        const itemCard = document.getElementById('gallery');
+
+        itemCard.innerHTML += `
+
+            <figure class="card-gallery">
+                <img crossorigin="anonymous" src="${allWork[i].imageUrl}" alt="${allWork[i].title}" />
+                <figcaption>${allWork[i].title}</figcaption>
+            </figure>`;
+    };
 };
 
+async function getCategories() {
+    let rep = await fetch("http://localhost:5678/api/categories");
+    let reponse = await rep.json();
+    return reponse;
+};
+
+const btnFilter = document.querySelector(".btn-filter");
+
+btnFilter.addEventListener("click", function () {
+    const workFilter = categories.filter(async function () {
+
+        await getCategories("http://localhost:5678/api/categories")
+        return [id = 2]
+    });
+
+});
 
 
-workdisplay();
+getWorks();
+generateCard();
+getCategories();
