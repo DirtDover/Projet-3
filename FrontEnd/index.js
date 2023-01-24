66b0b90e064ca6a80247751de0c92db85bf39d6d
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-01-24 11:45:28 +0100

    Début Modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -106,3 +106,45 @@ filtreHotel.addEventListener("click", () => {
 
 /* Création de la modale */
 
+let modal = null
+
+const openModal = function (e) {
+    e.preventDefault()
+    modal = document.querySelector(e.target.getAttribute("href"))
+    modal.style.display = null
+    modal.removeAttribute('aria-hidden')
+    modal.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener("click", closeModal)
+    modal.querySelector('.js-btn-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeAttribute('aria-modal')
+    modal.removeeventListener('click', closeModal)
+    modal.querySelector('.js-btn-close').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+
+
+
+document.querySelectorAll(".js_modal").forEach(a => {
+    a.addEventListener("click", openModal)
+
+})
+
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+})
